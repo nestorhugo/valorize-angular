27cf6e895a1845f1c344b82e0516e04baa9793ff
nestorhugo <nestorhugo@proton.me>
2025-02-16 02:49:35 -0300

    add primeicons dependency; update budget list item component with new styles and total expenses calculation

--- src/app/components/budget-list-item/budget-list-item.component.ts
+++ src/app/components/budget-list-item/budget-list-item.component.ts
@@ -1,14 +1,23 @@
 import { Component, Input } from "@angular/core";
 import { userSingleBudget } from "../../types/apiTypes";
 import { Card } from "primeng/card";
+import { CurrencyPipe, JsonPipe } from "@angular/common";
 
 @Component({
   selector: "app-budget-list-item",
   standalone: true,
-  imports: [Card],
+  imports: [Card, CurrencyPipe],
   templateUrl: "./budget-list-item.component.html",
   styleUrl: "./budget-list-item.component.scss",
 })
 export class BudgetListItemComponent {
   @Input() budget!: userSingleBudget;
+
+  ngOnInit() {
+    console.log(this.budget.gastos);
+  }
+
+  get totalBudgetExpenses() {
+    return this.budget.gastos.reduce((acc, gasto) => acc + gasto.valor, 0);
+  }
 }
